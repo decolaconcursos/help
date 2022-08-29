b130f1f16dcb5a0500b96327b80b6b8791ff235c
Lenderson Macedo <github@lenderson.com.br>
2022-08-29 19:24:37 -0400

    refactor:mantain only the needed setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { MDXProvider } from '@mdx-js/react'
 
-const Bold: React.FC<{children: React.ReactNode}> = ({ children }) => {
-  return (<div style={{ fontWeight: 300 }}>
-    {children}
-  </div>)
-}
-
-const components = {
-
-  Bold
-
-}
+const components = { }
 
 export default function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
   return (
