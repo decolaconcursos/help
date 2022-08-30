216ab1661542c409323edab8806ea22a2579589c
Lenderson Macedo <github@lenderson.com.br>
2022-08-29 20:25:52 -0400

    feat: GrayMatterAdapter should return question and content if success

---
+++ src/lib/adapters/gray-matter.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import path from 'path'
+import { Question } from '@/lib/models/questions'
+import matter from 'gray-matter'
+
+interface MatterAdapterResult {
+  question: Question
+  content?: string
+}
+
+export const matterAdapter = (filePath: string, fileName: string): MatterAdapterResult => {
+  const markdownWithMeta = fs.readFileSync(path.join(process.cwd(), filePath, fileName + '.mdx'), 'utf-8')
+  const { data: frontMatter, content } = matter(markdownWithMeta)
+  return {
+    question: frontMatter as Question,
+    content
+  }
+}
