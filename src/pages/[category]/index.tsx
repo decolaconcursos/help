415361c79b024ed3679d45c46b58c3d3ee04b011
Lenderson Macedo <github@lenderson.com.br>
2022-08-30 11:58:56 -0400

    feat: create Category and Home pages

--- src/pages/[category]/index.tsx
+++ src/pages/[category]/index.tsx
@@ -4,6 +4,9 @@ import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 
 import { getAllCategories } from '@/lib/get-all-categories'
 import { getQuestionByCategories } from '@/lib/get-question-by-categories'
+import { Header } from '@/components/Header'
+import { Question } from '@/lib/models/questions'
+import Link from 'next/link'
 
 export const getStaticPaths: GetStaticPaths = () => {
   const categories = getAllCategories()
@@ -41,14 +44,42 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   }
 }
 
-const Question: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ questions }) => {
+const QuestionPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ questions }) => {
+  const lenght = questions.length
   return (
 
-    <>
-      Categories
-    </>
+    <div className='min-h-s'>
+      <div className='absolute inset-0 bg-[#f9f5f1] z-0' />
+
+      <Header />
+      <div className='relative'>
+        <div className='max-w-5xl mx-auto px-6 py-12'>
+          <div className='flex justify-between items-baseline'>
+            <h1 className='text-4xl font-extrabold'>Categorias</h1>
+            <p>
+              {lenght} Perguntas
+            </p>
+          </div>
+          <div className='space-y-4 mt-16'>
+            {
+              questions.map((item: Question) => {
+                return (<Link href={`/pergunta/${item.slug ?? ''}`}>
+                  <div key={item.slug} className='w-full py-6 bg-white px-12 rounded-full'>
+                    <p>
+                      {item.title}
+                    </p>
+                  </div>
+                </Link>)
+              })
+            }
+          </div>
+
+        </div>
+
+      </div>
+    </div>
 
   )
 }
 
-export default Question
+export default QuestionPage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Header } from '@/components/Header'
 
-const Home: React.FC = () => <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
+const Home: React.FC = () => {
+  return (
+    <div className='relative min-h-screen'>
+      <div className='absolute inset-0 bg-gradient-to-b from-[#f9f5f1] to-white z-0' />
+      <Header />
+      <Hero />
+    </div>
+  )
+}
 
+const Hero: React.FC = () => {
+  const [,setFocus] = useState(false)
+
+  return (<div className='max-w-5xl mx-auto relative'>
+    <div className='px-4 md:py-6 space-y-4'>
+      <div className='text-center'>
+        <h4 className='text-3xl font-semibold'><span className='wave'>👋</span> Olá</h4>
+        <h1 className='text-4xl font-extrabold'>Como podemos te ajudar?</h1>
+        <p className='mt-2 font-semibold '>Loremzinho de boas, esle to aprdne minst farez isto</p>
+      </div>
+      <div className='flex flex-col gap-4 items-center w-full'>
+
+        <input type="text" onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} className='w-full max-w-xl px-6 py-3 rounded-lg advanced-shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#694D30] animated'/>
+
+        <div className='text-[#694D30]/80 font-semibold opacity-60 text-xs'>
+          Pressione <code className='snippet'>CTRL</code> ou <code className='snippet'>⌘</code> + <code className='snippet'>K</code> para mais
+        </div>
+      </div>
+      <div className='relative '>
+
+      </div>
+    </div>
+
+  </div>)
+}
 export default Home
