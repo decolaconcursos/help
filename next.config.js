d97719a48f94925cf9e98c83625bb8fa46865cff
Lenderson Macedo <github@lenderson.com.br>
2022-09-03 13:30:53 -0400

    feat: add Decola Layout automagic

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const glob = require('glob')
+const minimatch = require('minimatch')
 
 const { createLoader } = require('simple-functional-loader')
 const matter = require('gray-matter')
@@ -7,6 +8,10 @@ const matter = require('gray-matter')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
+
+const fallbackDefaultExports = {
+  'src/pages/decola/**/*': ['@/layouts/decola-layout', 'DecolaLayout']
+}
 
 module.exports = withBundleAnalyzer({
   async rewrites () {
@@ -33,9 +38,25 @@ module.exports = withBundleAnalyzer({
         createLoader(function (source) {
           const result = matter(source)
           const pageData = result.data
+
+          const extra = []
+          const resourcePath = path.relative(__dirname, this.resourcePath)
+
+          if (!/^\s*export\s+default\s+/m.test(source.replace(/```(.*?)```/gs, ''))) {
+            for (const glob in fallbackDefaultExports) {
+              if (minimatch(resourcePath, glob)) {
+                extra.push(
+                  `import { ${fallbackDefaultExports[glob][1]} as _Default } from '${fallbackDefaultExports[glob][0]}'`,
+                  'export default _Default'
+                )
+                break
+              }
+            }
+          }
           return [
             result.content,
-            `export const pageData = ${JSON.stringify(pageData)}`
+            ...extra,
+            `export const frontMatter = ${JSON.stringify(pageData)}`
           ].join('\n\n')
         })
       ]
