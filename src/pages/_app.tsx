37952e7ff24223b11884ba0ef491ca3ab27b5407
Lenderson Macedo <github@lenderson.com.br>
2022-09-03 14:30:03 -0400

    feat: add Progress Bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../style/global.css'
+import '../style/base.css'
 import React, { Fragment } from 'react'
 import { AppPropsWithLayout } from 'next/app'
+import { Router } from 'next/router'
 
-export default function MyApp ({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+  size: 2,
+  color: '#21A4C4',
+  className: 'bar-of-progress',
+  delay: 100
+})
+
+if (typeof window !== 'undefined') {
+  progress.start()
+  progress.finish()
+}
+
+Router.events.on('routeChangeStart', () => progress.start())
+Router.events.on('routeChangeComplete', () => progress.finish())
+Router.events.on('routeChangeError', () => progress.finish())
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout): JSX.Element => {
   const Layout = Component.Layout ?? Fragment
   const layoutProps = Component.layoutProps ?? {}
 
@@ -13,3 +32,5 @@ export default function MyApp ({ Component, pageProps }: AppPropsWithLayout): JS
 
   )
 }
+
+export default MyApp
