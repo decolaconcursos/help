ddcfd41dad665211327dc192d452c0aa81b00da3
Lenderson Macedo <github@lenderson.com.br>
2022-09-03 13:18:40 -0400

    chore: change some text

--- src/layouts/basic-layout.tsx
+++ src/layouts/basic-layout.tsx
@@ -7,7 +7,7 @@ export const BasicLayout: React.FC<Props> = ({ children }) => {
   return (
     <div>
       <div className={'text-2xl bg-sky-500'}>
-        asd
+        Basic Layout
       </div>
       {children}
     </div>
