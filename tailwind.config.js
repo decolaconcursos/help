62841456135a3f906071149bc06d34d651411a30
Lenderson Macedo <github@lenderson.com.br>
2022-09-03 16:57:29 -0400

    chore: add primary color on tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,22 @@ module.exports = {
     './src/components/**/*.{ts,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        ...colors,
+        primary: {
+          100: '#D0FAFC',
+          200: '#A1F3F7',
+          300: '#75E3F0',
+          400: '#62D7EA',
+          500: '#2CC2DD',
+          600: '#21A4C4',
+          700: '#2083A7',
+          800: '#1C607D',
+          900: '#1A4C65'
+        }
+      }
+    }
   },
   plugins: []
 
